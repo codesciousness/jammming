b1b84d8d537e01503f6182cf547aa1270e57ae24
Mia <micaelasgilbert@gmail.com>
2021-02-24 17:53:29 -0600

    Create static React components and pass down search results list to child components

---
+++ src/Components/App/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+const track = {
+  id: 'track_id',
+  name: 'song_name',
+  artist: 'artist_name',
+  album: 'album_name'
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [track, track, track],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Track.css';
+
+let isRemoval = true;
+
+class Track extends React.Component {
+    renderAction() {
+        return (
+            <button className="Track-action">
+                {isRemoval ? '-' : '+'}
+            </button>
+        );
+    }
+
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
+                </div>
+                <button className="Track-action">{isRemoval ? '-' : '+'}</button>
+            </div>
+        );
+    }
+}
+
+export default Track;
